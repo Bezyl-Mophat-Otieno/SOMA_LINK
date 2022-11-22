cc0e549535ddb42fff501f697204e4b898b58775
MugoBrian <mikanmuchiri@gmail.com>
2022-11-22 07:42:21 +0300

    Added create course form route with get method

--- backend/routes/coursesRoutes.js
+++ backend/routes/coursesRoutes.js
@@ -1,12 +1,13 @@
 
 const express= require('express');
 const router = express.Router()
-const {createCourse, getAllCourses, getMyCourses, joinCourse} = require('../controler/courseController');
+const {createCourse, getAllCourses, getMyCourses, joinCourse,createCourseForm} = require('../controler/courseController');
 const {ensureAuthenticated , forwardAuthenticated} = require ('../config/auth.js');
 
 
 router.post('/createCourse',ensureAuthenticated,createCourse);
 router.get('/coursesMarket',ensureAuthenticated,getAllCourses);
+router.get('/createCourseForm',createCourseForm);
 router.get('/MyCoures' ,ensureAuthenticated ,getMyCourses);
 router.post('/joinCourse/:id' ,ensureAuthenticated ,joinCourse);
 // router.put('/update/:id',ensureAuthenticated  ,updateGoal);
